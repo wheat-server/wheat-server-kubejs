bcd8e95d4b7f8b96cea61b660173d721b0970104
Frost-ZX <30585462+Frost-ZX@users.noreply.github.com>
2022-08-20 14:27:34 +0800

    feat: 添加颜色列表

--- kubejs/probe/user/constants.d.ts
+++ kubejs/probe/user/constants.d.ts
@@ -1,5 +1,13 @@
 declare const global: {
 
+  /** 颜色列表 */
+  COLORS: {
+    [key: string]: {
+      CODE: string,
+      HEX: string,
+    };
+  };
+
   /** 日志前缀 */
   LOG_PREFIX: string;
 
--- kubejs/startup_scripts/_main_.js
+++ kubejs/startup_scripts/_main_.js
@@ -1,5 +1,26 @@
 // priority: 100
 
+// 参考：
+// https://github.com/Railcraft/Railcraft/blob/mc-1.12.2/src/main/java/mods/railcraft/common/plugins/color/EnumColor.java
+global.COLORS = {
+  WHITE: { CODE: 'white', HEX: '#F9FFFE' },
+  ORANGE: { CODE: 'orange', HEX: '#F9801D' },
+  MAGENTA: { CODE: 'magenta', HEX: '#C74EBD' },
+  LIGHT_BLUE: { CODE: 'light_blue', HEX: '#3AB3DA' },
+  YELLOW: { CODE: 'yellow', HEX: '#FED83D' },
+  LIME: { CODE: 'lime', HEX: '#80C71F' },
+  PINK: { CODE: 'pink', HEX: '#F38BAA' },
+  GRAY: { CODE: 'gray', HEX: '#474F52' },
+  LIGHT_GRAY: { CODE: 'light_gray', HEX: '#9D9D97' },
+  CYAN: { CODE: 'cyan', HEX: '#169C9C' },
+  PURPLE: { CODE: 'purple', HEX: '#8932B8' },
+  BLUE: { CODE: 'blue', HEX: '#3C44AA' },
+  BROWN: { CODE: 'brown', HEX: '#835432' },
+  GREEN: { CODE: 'green', HEX: '#5E7C16' },
+  RED: { CODE: 'red', HEX: '#B02E26' },
+  BLACK: { CODE: 'black', HEX: '#1D1D21' },
+};
+
 global.LOG_PREFIX = '[KubeJS]';
 
 console.info(`${global.LOG_PREFIX} 启动`);
